7e1ea1a87e38fb3941d6c9b11b8c27ac12dd4b89
Jason Hall <jason@chainguard.dev>
2023-09-01 17:51:20 -0400

    moved image-copy-ecr to enforce-events
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- image-copy-ecr/iac/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "url" {
-  value = aws_lambda_function_url.lambda.function_url
-}
-
-output "dst_repo" {
-  value = aws_ecr_repository.repo.repository_url
-}
--- image-copy-ecr/iac/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "group" {
-  type        = string
-  description = "The Chainguard group that we are subscribing to."
-}
-
-variable "dst_repo" {
-  type        = string
-  description = "The destination repo where images should be copied to."
-}
